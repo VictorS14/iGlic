34308975d185a80a42f7af2459744b4acbb0fd4d
Victor Moura <victormoura310@gmail.com>
2026-01-08 15:47:35 -0300

    feat: Criar função para atualizar uma medição de glicose

--- backend/services/glucoseRepository.js
+++ backend/services/glucoseRepository.js
@@ -1,9 +1,21 @@
-import * as repositories from "../repositories/useRepository.js";
+import * as glicoseRepo from "../repositories/useRepository.js";
 
 export const registerGlucose = async (value, userId) => {
 	if(value < 10 || value > 700) {
 		throw new Error("Valor de glicemia inválido.");
 	}
 
-	return await repositories.saveGlicoseTest(value, userId);
+	return await glicoseRepo.saveGlicoseTest(value, userId);
+}
+
+export const updateMeasurement = async (glicoseValue, glicoseId, userId) => {
+	if(!glicoseValue || glicoseValue <= 0) {
+		throw new Error("O valor de glicose deve ser positivo!")
+	}
+
+	const updated = await glicoseRepo.updateGlicoseValue(glicoseValue, glicoseId, userId);
+
+	if(!updated) throw new Error("Registro não encontrado.")
+
+	return updated; 
 }
