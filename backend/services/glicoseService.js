32e5a55819e037cd77b521633cb7112be08fe284
Victor Moura <victormoura310@gmail.com>
2026-01-16 21:26:35 -0300

    refactor: Mudar o nome do arquivo para glicoseService

---
+++ backend/services/glicoseService.js
@@ -0,0 +1,51 @@
+import * as glicoseRepo from "../repositories/useRepository.js";
+
+export const registerGlucose = async (value, userId) => {
+	if(value < 10 || value > 700) {
+		throw new Error("Valor de glicemia inválido.");
+	}
+
+	return await glicoseRepo.saveGlicoseTest(value, userId);
+}
+
+export const updateMeasurement = async (glicoseValue, glicoseId, userId) => {
+	if(!glicoseValue || glicoseValue <= 0) {
+		throw new Error("O valor de glicose deve ser positivo!")
+	}
+
+	const updated = await glicoseRepo.updateGlicoseValue(glicoseValue, glicoseId, userId);
+
+	if(!updated) throw new Error("Registro não encontrado.")
+
+	return updated; 
+}
+
+
+export const getAverage = async (userId) => {
+	const data = await glicoseRepo.getGlicoseAverageDay(userId);
+
+	return {
+		userId,
+		average: data.media_do_dia || 0
+	}
+}
+
+
+export const getAverageByPeriod = async (userId, days) => {
+	if(days > 30) {
+		throw new Error("Para proteger o servidor o limite máximo é de 30 dias");
+	}
+
+	const data = await glicoseRepo.getGlicoseAverageByPeriod(userId, days);
+
+	return {
+		userId,
+		period: `${days} days`,
+		average: data.media || 0
+	};
+}
+
+export const deleteMeasurement = async  (glicoseId, userId) => {
+
+	return glicoseRepo.deleteById(glicoseId, userId);
+}
