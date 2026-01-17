9bc598130898db10aeda8159e695c53215177b6d
Victor Moura <victormoura310@gmail.com>
2026-01-16 21:57:09 -0300

    feat: Criar controller para pegar e media do dia

--- backend/controllers/glicoseController.js
+++ backend/controllers/glicoseController.js
@@ -24,4 +24,16 @@ export const update = async (req, res) => {
 	}
 };
 
+export const average = async (req, res) => {
+	try {
+		const { userId } = req.body;
+		const data = await glicoseService.getAverage(userId);
+
+		return res.status(200).json(data)
+	} catch(error) {
+
+		return res.status(404).json({ error: error.message });
+	}
+}
+
 
