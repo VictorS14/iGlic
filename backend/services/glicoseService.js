07ff3418e4cc87dcd1798e49938cb5e3475b8b8d
Victor Moura <victormoura310@gmail.com>
2026-02-03 11:19:56 -0300

    feat: passar measure_at para register e update

--- backend/services/glicoseService.js
+++ backend/services/glicoseService.js
@@ -1,19 +1,19 @@
 import * as glicoseRepo from "../repositories/useRepository.js";
 
-export const registerGlucose = async (value, userId) => {
+export const registerGlucose = async (value, userId, measure_at) => {
 	if(value < 10 || value > 700) {
 		throw new Error("Valor de glicemia inválido.");
 	}
 
-	return await glicoseRepo.saveGlicoseTest(value, userId);
+	return await glicoseRepo.saveGlicoseTest(value, userId, measure_at);
 }
 
-export const updateMeasurement = async (glicoseValue, glicoseId, userId) => {
+export const updateMeasurement = async (glicoseValue, glicoseId, userId, measure_at) => {
 	if(!glicoseValue || glicoseValue <= 0) {
 		throw new Error("O valor de glicose deve ser positivo!")
 	}
 
-	const updated = await glicoseRepo.updateGlicoseValue(glicoseValue, glicoseId, userId);
+	const updated = await glicoseRepo.updateGlicoseValue(glicoseValue, glicoseId, userId, measure_at);
 
 	if(!updated) throw new Error("Registro não encontrado.")
 
