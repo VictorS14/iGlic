22a40ac27619b1a85d6f8c2ddcc63ae996b15a6f
Victor Moura <victormoura310@gmail.com>
2026-02-12 20:44:13 -0300

    feat: passar a nova rota authRoutes para o server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
 import glicoseRoutes from './routes/glicoseRoutes.js'
+import authRoutes from './routes/authRoutes.js'; // Importa as novas rotas de autenticação
 
 const app = express();
 app.use(express.json());
 
 app.use(glicoseRoutes);
+app.use(authRoutes);
 
 const PORT = 3000;
 
