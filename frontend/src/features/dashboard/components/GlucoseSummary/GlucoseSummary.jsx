c57dcc9700513ee7d872022246d138e08259f75e
Victor Moura <victormoura310@gmail.com>
2026-03-12 22:49:22 -0300

    feat: Adicionar Timer ranger selector component

---
+++ frontend/src/features/dashboard/components/GlucoseSummary/GlucoseSummary.jsx
@@ -0,0 +1,9 @@
+import { TimerRangerSelector } from "./TimerRangerSelector"
+
+export const GlucoseSummary = () => {
+  return (
+    <div className="w-full h-44 flex justify-center">
+        <TimerRangerSelector/>
+    </div>
+  )
+}
