339c644aad7d917e7cc192d6c6d6e060dfa6db67
Victor Moura <victormoura310@gmail.com>
2026-03-12 22:54:51 -0300

    feat: Adicionar o Dashboard ao main content

--- frontend/src/layout/Content.jsx
+++ frontend/src/layout/Content.jsx
@@ -1,9 +1,13 @@
-import { Header } from "./Header"
+import { Header } from "./Header";
+import { Dashboard } from "../features/dashboard/pages/Dashboard";
 
 export const Content = () => {
   return (
-    <div className="w-full h-screen flex bg-amber-600">
+    <div className="w-full h-screen flex flex-col items-center bg-gray-100">
         <Header/>
+        <main className="w-full max-w-3xl p-4 grow border">
+            <Dashboard/>
+        </main>
     </div>
   )
 }
