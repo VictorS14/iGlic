81a823b967ff2c83ae77845c9cbbcb99b6b201d4
Victor Moura <victormoura310@gmail.com>
2026-03-19 13:23:04 -0300

    Adicionado o CORS

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+import cors from "cors"
 import express from 'express';
 import 'dotenv/config';
 import glicoseRoutes from './routes/glicoseRoutes.js'
-import authRoutes from './routes/authRoutes.js'; // Importa as novas rotas de autenticação
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(glicoseRoutes);
 app.use(authRoutes);
