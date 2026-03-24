2da605a9d2a87f30877408c31d3efcb3230ec430
Victor Moura <victormoura310@gmail.com>
2026-03-24 11:59:40 -0300

    mudar nome da prop ranger para period

--- frontend/src/features/dashboard/ui/ButtonTimerSelector.jsx
+++ frontend/src/features/dashboard/ui/ButtonTimerSelector.jsx
@@ -1,9 +1,12 @@
-export const ButtonTimerSelector = ({ranger, handlerSelect, selected}) => {
+export const ButtonTimerSelector = ({ period, handlerSelect, selected }) => {
   return (
-    <button 
-        className={`${selected === ranger ? "bg-gray-500" : ""} flex w-1/3 h-full flex-row items-center justify-center border outline-none`}
-        onClick={() => handlerSelect(ranger)}>
-            <p className={`${selected === ranger ? "text-white" : "text-black"}`}>{ranger}</p> 
+    <button
+      className={`${selected === period ? "bg-gray-500" : ""} flex w-1/3 h-full flex-row items-center justify-center border outline-none`}
+      onClick={() => handlerSelect(period)}
+    >
+      <p className={`${selected === period ? "text-white" : "text-black"}`}>
+        {period}
+      </p>
     </button>
   );
 };
