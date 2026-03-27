fb9838232c927180b91574162c2adedc3078a244
Victor Moura <victormoura310@gmail.com>
2026-03-27 10:47:18 -0300

    Adicionar Routes e definir os caminho para Dashboard e settings

--- frontend/src/layout/Content.jsx
+++ frontend/src/layout/Content.jsx
@@ -1,13 +1,18 @@
 import { Header } from "./Header";
 import { Dashboard } from "../features/dashboard/pages/Dashboard";
+import { Routes, Route } from "react-router-dom";
+import { SettingsPage } from "../features/settings/setting.jsx";
 
 export const Content = () => {
   return (
     <div className="w-full h-screen flex flex-col items-center bg-gray-100">
-        <Header/>
-        <main className="w-full max-w-3xl p-4 grow border">
-            <Dashboard/>
-        </main>
+      <Header />
+      <main className="w-full max-w-3xl p-4 grow border">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </main>
     </div>
-  )
-}
+  );
+};
