283e565c25d81c0b3c19ad29c0467107f2c01775
Victor Moura <victormoura310@gmail.com>
2026-04-13 10:26:52 -0300

    Criando store para salvar a faixa alvo de controle

---
+++ frontend/src/features/settings/store/useTargetRange.js
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+export const useTargetRange = create(
+devtools((set) => ({
+   veryHigh: 0,
+   minTarget: 0,
+   maxTarget: 0,
+   setMinTarget: (value) => {
+      set({ minTarget: value }, false, "setMinTarget");
+   },
+
+   setMaxTarget: (value) => {
+      set({ maxTarget: value }, false, "setMaxTarget");
+   }
+})));
