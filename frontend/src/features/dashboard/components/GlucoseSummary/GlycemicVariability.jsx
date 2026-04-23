a55e0b295896208fbd52aa60e0fed5f103e4b6ac
Victor Moura <victormoura310@gmail.com>
2026-04-23 18:01:38 -0300

    feat: Contando altas e baixas de leituras de glicose usando o target range do usuário

--- frontend/src/features/dashboard/components/GlucoseSummary/GlycemicVariability.jsx
+++ frontend/src/features/dashboard/components/GlucoseSummary/GlycemicVariability.jsx
@@ -1,21 +1,26 @@
-export const GlycemicVariability = () => {
+export const GlycemicVariability = ({ medments }) => {
   const userSetting = JSON.parse(localStorage.getItem("userSettings"));
 
-  console.log(userSetting);
+  const lowCount = medments?.filter(
+    (values) => values < userSetting.targetRangeMin,
+  ).length;
+  const highCount = medments?.filter(
+    (values) => values > userSetting.targetRangeMax,
+  ).length;
 
   return (
     <div className="w-1/2 h-1/2 flex items-center justify-center">
       <div className="border-r pr-2">
         <span className="text-[min(5vw,1.25rem)]">Baixas</span>
         <p className="text-green-500 text-2xl">
-          2 <span className="text-[1rem]">Hipos</span>
+          {lowCount} <span className="text-[1rem]">Hipos</span>
         </p>
       </div>
 
       <div className="border-l pl-2">
         <span className="text-[min(5vw,1.25rem)]">Altas</span>
         <p className="text-red-500 text-2xl">
-          3 <span className="text-[1rem]">Hiper</span>
+          {highCount} <span className="text-[1rem]">Hiper</span>
         </p>
       </div>
     </div>
