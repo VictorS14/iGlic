189ae411af59afb9b374d1e8b53dd8810ff33851
Victor Moura <victormoura310@gmail.com>
2026-05-19 16:06:22 -0300

    Torna o Header fixo no top

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -1,13 +1,12 @@
-import { CurtainMenu } from '../components/CurtainMenu';
+import { CurtainMenu } from "../components/CurtainMenu";
 
 export const Header = () => {
   return (
-    <header className="w-full max-w-3xl h-16 bg-white flex items-center shadow-sm px-4 border-b border-gray-100">
+    <header className="w-full fixed top-0 left-0 z-50 max-w-3xl h-16 bg-white flex items-center shadow-sm px-4 border-b border-gray-100">
       <div className="w-full h-full flex items-center justify-between">
         <h1 className="text-2xl font-bold text-green-600 font-serif">iGlic</h1>
-        <CurtainMenu/>
+        <CurtainMenu />
       </div>
     </header>
-  )
-}
-
+  );
+};
