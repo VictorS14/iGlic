e53d5169a22afd2bb1b89b3876fa9a8854a17f7c
Victor Moura <victormoura310@gmail.com>
2026-05-21 20:32:06 -0300

    feat(hooks): cria hook de mutação para salvar novas medições de glicose

---
+++ frontend/src/features/dashboard/hooks/useSaveGlicose.jsx
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+export const useSaveGlicose = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({ userId, value, dateTime }) => {
+      const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
+      const baseUrl = `${apiUrl}/glicose`;
+
+      const response = await axios.post(baseUrl, {
+        userId,
+        glicoseValue: value,
+        measure_at: dateTime
+      });
+
+      return response.data;
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["recentReadings"] });
+    },
+  });
+};
