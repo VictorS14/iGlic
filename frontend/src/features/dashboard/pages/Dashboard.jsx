7169d84140f8e10f3ea03bc31f44908dbd0d8352
Victor Moura <victormoura310@gmail.com>
2026-05-20 21:48:35 -0300

    Adiciona logica para abrir o modal

--- frontend/src/features/dashboard/pages/Dashboard.jsx
+++ frontend/src/features/dashboard/pages/Dashboard.jsx
@@ -1,11 +1,27 @@
+import {useState} from "react";
 import { GlucoseSummary } from "../components/GlucoseSummary/GlucoseSummary";
 import { RecentReadings } from "../components/RecentReadings/RecentReadings";
+import { NewEntryModal } from "../ui/NewEntryModal";
+import { AddGlucoseModal } from "../components/AddGlucoseModal/AddGlucoseModal";
 
 export const Dashboard = () => {
+const [isOpen, setIsOpen] = useState(false);
+
+const handleToggleModal = () => {
+  setIsOpen(!isOpen)
+}
+
   return (
-    <div className="w-full h-full border flex flex-col gap-2">
-        <GlucoseSummary/>
-        <RecentReadings/>
+    <div className="w-full min-h-dvh border flex flex-col gap-2">
+      <GlucoseSummary />
+      <RecentReadings />
+      <AddGlucoseModal 
+      isOpen={isOpen}
+      handleToggleModal={handleToggleModal}/>
+      <NewEntryModal 
+      isOpen={isOpen}
+      handleToggleModal={handleToggleModal}
+      />
     </div>
   );
 };
