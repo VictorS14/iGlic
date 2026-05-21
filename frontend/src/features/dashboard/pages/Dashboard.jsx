8a81cb25b1230f0dcbd9224fabe10a712aae2f83
Victor Moura <victormoura310@gmail.com>
2026-05-21 20:25:49 -0300

    feat(dashboard): Adiciona componente de nova entrada na página principal

--- frontend/src/features/dashboard/pages/Dashboard.jsx
+++ frontend/src/features/dashboard/pages/Dashboard.jsx
@@ -1,27 +1,13 @@
-import {useState} from "react";
 import { GlucoseSummary } from "../components/GlucoseSummary/GlucoseSummary";
 import { RecentReadings } from "../components/RecentReadings/RecentReadings";
-import { NewEntryModal } from "../ui/NewEntryModal";
-import { AddGlucoseModal } from "../components/AddGlucoseModal/AddGlucoseModal";
+import { AddGlucoseEntry } from "../components/AddGlucoseEntry";
 
 export const Dashboard = () => {
-const [isOpen, setIsOpen] = useState(false);
-
-const handleToggleModal = () => {
-  setIsOpen(!isOpen)
-}
-
   return (
     <div className="w-full min-h-dvh border flex flex-col gap-2">
       <GlucoseSummary />
       <RecentReadings />
-      <AddGlucoseModal 
-      isOpen={isOpen}
-      handleToggleModal={handleToggleModal}/>
-      <NewEntryModal 
-      isOpen={isOpen}
-      handleToggleModal={handleToggleModal}
-      />
+      <AddGlucoseEntry/>
     </div>
   );
 };
