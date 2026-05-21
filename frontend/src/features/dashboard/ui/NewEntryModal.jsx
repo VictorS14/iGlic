248a6a01c8017782abce864916b7b71fbd86bed1
Victor Moura <victormoura310@gmail.com>
2026-05-20 21:46:43 -0300

    Usa props para abrir o modal

--- frontend/src/features/dashboard/ui/NewEntryModal.jsx
+++ frontend/src/features/dashboard/ui/NewEntryModal.jsx
@@ -1,8 +1,10 @@
 import { FaPlus } from "react-icons/fa6";
 
-export const NewEntryModal = () => {
+export const NewEntryModal = ({isOpen, handleToggleModal}) => {
   return (
-   <button className="fixed bottom-6 right-6 w-14 h-14 bg-blue-600 text-white rounded-full flex items-center justify-center shadow-lg hover:bg-blue-700 transition-all active:scale-95 z-50 cursor-pointer">
+   <button 
+   className={`${isOpen ? "hidden" : "block"} fixed bottom-6 right-6 w-14 h-14 bg-blue-600 text-white rounded-full flex items-center justify-center shadow-lg hover:bg-blue-700 transition-all active:scale-95 z-50 cursor-pointer`}
+   onClick={handleToggleModal}>
       <FaPlus size={24} />
    </button> 
   )
