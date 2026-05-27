269512f01a97834d1a6fabc2d2b0509250f0d5f2
Victor Moura <victormoura310@gmail.com>
2026-05-26 21:48:23 -0300

    Add um state global ao inves de local

--- frontend/src/features/dashboard/components/AddGlucoseEntry.jsx
+++ frontend/src/features/dashboard/components/AddGlucoseEntry.jsx
@@ -1,23 +1,25 @@
-import {useState} from "react";
 import { NewEntryModal } from "../ui/NewEntryModal";
 import { AddGlucoseModal } from "./AddGlucoseModal/AddGlucoseModal";
+import { useModalEntry } from "../store/useModalEntry";
 
 export const AddGlucoseEntry = () => {
-   const [isOpen, setIsOpen] = useState(false);
-   
-   const handleToggleModal = () => {
-     setIsOpen(!isOpen);
-   }
+  const setterIsOpen = useModalEntry((state) => state.setIsOpen);
+  const isOpen = useModalEntry((state) => state.isOpen);
+
+  const handleToggleModal = () => {
+    setterIsOpen(!isOpen);
+  };
 
   return (
     <>
       <AddGlucoseModal 
-      isOpen={isOpen}
-      handleToggleModal={handleToggleModal}/>
-      <NewEntryModal 
-      isOpen={isOpen}
-      handleToggleModal={handleToggleModal}
+      isOpen={isOpen} 
+      handleToggleModal={handleToggleModal} 
       />
+      <NewEntryModal 
+      isOpen={isOpen} 
+      handleToggleModal={handleToggleModal}
+       />
     </>
-  )
-}
+  );
+};
