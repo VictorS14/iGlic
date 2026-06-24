cc376d25a78480e3b6aa5a967bb75e2a8f64de77
Victor Moura <victormoura310@gmail.com>
2026-06-24 16:53:56 -0300

    style: ajusta o posicionamento dos dados de GlycemicVariability

--- frontend/src/features/dashboard/components/GlucoseSummary/GlycemicVariability.jsx
+++ frontend/src/features/dashboard/components/GlucoseSummary/GlycemicVariability.jsx
@@ -15,15 +15,15 @@ export const GlycemicVariability = ({ medments }) => {
 
   return (
     <div className="w-1/2 h-1/2 flex items-center justify-center border">
-      <div className="border-r pr-2">
-        <span className="text-[min(5vw,1.25rem)]">Baixas</span>
-        <p className="text-green-500 text-2xl font-bold">
+      <div className="w-full flex flex-col pl-2 border-r">
+        <span className="text-[min(5vw,1.5rem)]">Baixas</span>
+        <p className="text-yellow-500 text-2xl font-bold">
           {lowCount} <span className="text-[1rem] font-normal">Hipos</span>
         </p>
       </div>
 
-      <div className="border-l pl-2">
-        <span className="text-[min(5vw,1.25rem)]">Altas</span>
+      <div className="w-full flex flex-col border-l pl-2">
+        <span className="text-[min(5vw,1.5rem)]">Altas</span>
         <p className="text-red-500 text-2xl font-bold">
           {highCount} <span className="text-[1rem] font-normal">Hiper</span>
         </p>
