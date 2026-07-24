5cd8f13730933761eaaddd620397d7e99491a27e
victor moura <victormoura310@gmail.com>
2026-07-23 22:03:26 -0300

    Altera a rota localhost do frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    port: 4122,
+  },
   plugins: [
     react(),
     tailwindcss()
