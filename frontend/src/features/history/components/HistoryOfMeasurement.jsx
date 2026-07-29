15bcdceb2fa07c9c9091631808c67371d1e46008
victor moura <victormoura310@gmail.com>
2026-07-28 21:24:53 -0300

    Cria conjuntos de IDs para permitir que os wrappers estejam abertos simultaneamente

--- frontend/src/features/history/components/HistoryOfMeasurement.jsx
+++ frontend/src/features/history/components/HistoryOfMeasurement.jsx
@@ -4,20 +4,29 @@ import { WrapperOfMeasurements } from "./WrapperOfMeasurements";
 import { useAuth } from "../../../context/AuthContext.jsx";
 
 export const HistoryOfMeasurement = () => {
-  const [openItemId, setItemOpenId] = useState(null);
+  const [openItemIds, setOpenItemIds] = useState(new Set());
   const { user } = useAuth();
   const userId = user?.id;
   const { data } = useAllData(userId);
 
   const handleToggle = (itemId) => {
-    setItemOpenId(openItemId === itemId ? null : itemId);
+    setOpenItemIds((prev) => {
+      const newSet = new Set(prev);
+
+      if(newSet.has(itemId)){
+        newSet.delete(itemId);
+      }else {
+        newSet.add(itemId);
+      }
+      return newSet;
+    });
   };
 
   return (
     <div className=" flex flex-col gap-3">
       {data?.map((item) => (
         <WrapperOfMeasurements 
-        openItemId={openItemId}
+        openItemIds={openItemIds}
         handleToggle={handleToggle}
         {...item}
         />
