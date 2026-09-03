1df4a1d7db862649cf27972bf62e09641fcb45f4
victor moura <victormoura310@gmail.com>
2026-09-03 20:05:28 -0300

    Add max-w-6xl mx-auto px-4 para não esticar em telas gigantes

--- frontend/src/layout/Content.jsx
+++ frontend/src/layout/Content.jsx
@@ -2,7 +2,8 @@ import { Header } from "./Header";
 
 export const Content = ({ children }) => {
   return (
-    <div className="w-full min-h-dvh flex flex-col items-center bg-gray-100">
+    <div className="w-full min-h-dvh flex flex-col items-center bg-gray-100 
+    lg:max-w-6xl lg:mx-auto lg:px-4">
       <Header />
       <main className="w-full mt-16 max-w-7xl p-4 grow">
         {children}
