5109a95089ff8baf7efae38096bb5eb5afe0ec64
victor moura <victormoura310@gmail.com>
2026-09-03 20:29:03 -0300

    Add hover no brackpoint lg:1024px

--- frontend/src/features/dashboard/ui/ButtonTimerSelector.jsx
+++ frontend/src/features/dashboard/ui/ButtonTimerSelector.jsx
@@ -1,7 +1,7 @@
 export const ButtonTimerSelector = ({ period, handlerSelect, selected, customClassName }) => {
   return (
     <button
-      className={`${selected === period ? "bg-gray-500" : ""} flex w-1/3 h-full flex-row items-center justify-center outline-none ${customClassName}`}
+      className={`${selected === period ? "bg-gray-500" : ""} flex w-1/3 h-full flex-row items-center justify-center outline-none ${customClassName} lg:hover:bg-gray-400`}
       onClick={() => handlerSelect(period)}
     >
       <p className={`${selected === period ? "text-white" : "text-black"}`}>
