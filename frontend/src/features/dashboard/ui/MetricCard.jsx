dfea9b4c555947f79a7555a36ad54f8fad2add68
victor moura <victormoura310@gmail.com>
2026-09-03 20:15:12 -0300

    Add shadow-sm, rounded-lg e bg-white

--- frontend/src/features/dashboard/ui/MetricCard.jsx
+++ frontend/src/features/dashboard/ui/MetricCard.jsx
@@ -1,7 +1,7 @@
 export const MetricCard = ({ title, value }) => {
   return (
-    <div className="w-1/2 h-1/2 flex flex-col p-2 border">
-      <h3>{title}</h3>
+    <div className="w-1/2 h-1/2 flex flex-col p-2 shadow-sm rounded-lg bg-white">
+      <span>{title}</span>
       <data className="text-2xl font-bold">{value}</data>
     </div>
   );
